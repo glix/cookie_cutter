3a881d8cc5bdacf94571e8c7d80475217ba30447
Danny Glix <dannyglix@gmail.com>
2013-07-12 10:15:09 +0530

    Cookie Cutter

---
+++ common.js
@@ -0,0 +1,33 @@
+  
+  function setCookie(c_name,value,exdays)
+         {
+                    var exdate=new Date();
+                    exdate.setDate(exdate.getDate() + exdays);
+                    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+                    document.cookie=c_name + "=" + c_value;
+         }
+         function getCookie(c_name)
+        {
+            var i,x,y,ARRcookies=document.cookie.split(";");
+            for (i=0;i<ARRcookies.length;i++)
+            {
+              x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+              y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+              x=x.replace(/^\s+|\s+$/g,"");
+              if (x==c_name)
+                {
+                return unescape(y);
+                }
+              }
+        }
+        function CheckId(){
+            CookieValue=getCookie("CookieValue");
+            if(document.getElementById('CookieVal').value=="decline"){
+                document.getElementById('Calculator').style.display="none";
+                document.getElementById('LifeInsurance').style.display="none";
+				setCookie("CookieValue","decline",365);
+            }else if(CookieValue=='decline'){
+                document.getElementById('Calculator').style.display="none";
+                document.getElementById('LifeInsurance').style.display="none";
+			}
+        }
